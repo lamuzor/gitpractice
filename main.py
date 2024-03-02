67005e67046f3f3f3b62523644dd77a861d31659
lamuzor <nyashamuzoriana@gmail.com>
2024-03-02 15:04:45 +0200

    new files updated

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import module
+my_list = [i+1 for i in range(20)]
+
+print(module.suml(my_list) )
+print(module.prodl(my_list))
+
+print('*'*15)
+
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))
+
